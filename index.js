e5285996ec8cd4fef1f2c46665bc37883adec8d3
aqsa <aqsa@gmail.com>
2022-09-23 13:25:44 +0500

    merging change

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ try {
 } catch (e) {
   console.error(e)
 }
+
+try {
+  const StaticmanAPI = require('./server')
+  const api = new StaticmanAPI()
+
+  api.start(port => {
+    console.log('Staticman API running on port', port)
+  })
+} catch (e) {
+  console.error(e)
+}
+
